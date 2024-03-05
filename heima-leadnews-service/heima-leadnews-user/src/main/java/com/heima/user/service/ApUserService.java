7bde359532abd052971925f557c7d010ca7eb635
gg <164349532@qq.com>
2024-03-05 15:55:28 +0800

    login commit

---
+++ heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/service/ApUserService.java
@@ -0,0 +1,13 @@
+package com.heima.user.service;
+
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.heima.model.common.dtos.LoginDto;
+import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.common.user.pojos.ApUser;
+
+public interface ApUserService extends IService<ApUser> {
+
+
+    ResponseResult login(LoginDto dto);
+}
